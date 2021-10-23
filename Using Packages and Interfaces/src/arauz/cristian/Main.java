59c175fd7b950a2116ce2373064662014516e6a7
carauz2 <90482295+carauz2@users.noreply.github.com>
2021-10-22 18:06:37 -0700

    Add files via upload

---
+++ Using Packages and Interfaces/src/arauz/cristian/Main.java
@@ -0,0 +1,17 @@
+package arauz.cristian;
+
+import animal.carnivore.Lion;
+import animal.herbivore.Giraffe;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Lion lion = new Lion();
+		Giraffe giraffe = new Giraffe();
+		
+		System.out.println(lion.speak());
+		System.out.println(giraffe.speak());
+	}
+
+}
---
+++ Using Packages and Interfaces/src/arauz/cristian/package-info.java
@@ -0,0 +1,1 @@
+package arauz.cristian;
